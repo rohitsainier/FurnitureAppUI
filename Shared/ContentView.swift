f59da330714776e84fc7cb9db78a6017f5bffce7
rohitsainier <rohitsainier@gmail.com>
2022-04-22 00:15:40 +0530

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Rohit Saini on 22/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/SwiftUI_by_SBSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_by_SBSApp.swift
+//  Shared
+//
+//  Created by Rohit Saini on 22/04/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_by_SBSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

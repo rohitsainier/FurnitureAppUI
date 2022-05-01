8e5e9fe9311ad4479fb4a4baaf3b5d3a35590ad0
rohitsainier <rohitsainier@gmail.com>
2022-05-01 23:20:56 +0530

    first commit

---
+++ Shared/DetailScreen.swift
@@ -0,0 +1,20 @@
+//
+//  DetailScreen.swift
+//  SwiftUI_by_SBS (iOS)
+//
+//  Created by Rohit Saini on 01/05/22.
+//
+
+import SwiftUI
+
+struct DetailScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailScreen()
+    }
+}
---
+++ Shared/HomeScreen.swift
@@ -0,0 +1,20 @@
+//
+//  HomeScreen.swift
+//  SwiftUI_by_SBS (iOS)
+//
+//  Created by Rohit Saini on 01/05/22.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeScreen()
+    }
+}
